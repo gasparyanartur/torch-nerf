19e0f4783c3981a4d7f6c84cba8f9a4b3a10c2f4
linhara <linus_haraldsson@hotmail.com>
2023-10-14 17:32:04 +0200

    medium nerf model

--- src/model.py
+++ src/model.py
@@ -32,5 +32,51 @@ class TestNet(nn.Module):
 
         return rgb, sigma
 
+class MediumNet(nn.Module):
+    def __init__(self, L1, L2, n_components, n_hidden):
+        super().__init__()
+
+        self.d1 = n_components * 2 * L1
+        self.d2 = n_components * 2 * L2
+        self.l5 = n_hidden + self.d2
+
+        self.lin1 = nn.Linear(self.d1, n_hidden)
+        self.lin2 = nn.Linear(n_hidden, n_hidden)
+        self.lin3 = nn.Linear(n_hidden, n_hidden)
+        self.lin4 = nn.Linear(n_hidden , n_hidden+1)     
+        self.lin5 = nn.Linear(n_hidden + self.d2, n_hidden//2)
+        self.lin6 = nn.Linear(n_hidden//2 , 3)
+
+    def forward(self, o: torch.Tensor, d: torch.Tensor) -> torch.Tensor:
+        # o: <B, NS, D*2*L>
+        # d: <B, NS, D*2*L>
+        assert len(o.shape) == 3
+        assert len(d.shape) == 3
+        assert o.size(2) == self.d1
+        assert d.size(2) == self.d2
+
+        x = self.lin1(o)
+        x = nn.functional.relu(x)
+        x = self.lin2(x)
+        x = nn.functional.relu(x)
+        x = self.lin3(x)
+        x = nn.functional.relu(x)        
+        x = self.lin4(x)
+
+        # no activation func according to paper
+        sigma = x[..., 0]  # <B, NS>
+        x = torch.cat((d, x[..., 1:]), dim=2)
+        x = self.lin5(x)
+        x = nn.functional.relu(x)
+        rgb = self.lin6(x)  # <B, NS, 3>
+
+        sigma = nn.functional.relu(sigma)
+        rgb = nn.functional.sigmoid(rgb)
+
+        return rgb, sigma
+    
+class NeRFnet(nn.Module):
+    def __init__(self, L1, L2, n_components, n_hidden):
+        super().__init__()
 
 
